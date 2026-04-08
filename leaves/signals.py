8d3ad643781747e11de58ce4ee4dfa35ad92dce1
gujjarrk908 <gujjarrk908@gmail.com>
2026-04-08 10:34:32 +0530

    Login bug fix

--- leaves/signals.py
+++ leaves/signals.py
@@ -2,9 +2,18 @@ from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
 from .models import UserSession
 
+from django.contrib.sessions.models import Session
+
 @receiver(user_logged_in)
 def manage_user_session(sender, request, user, **kwargs):
-    # After user logs in, store their new session key
+    # Proactively logout other sessions for this user (Auto-Logout)
+    try:
+        previous_session = UserSession.objects.get(user=user)
+        Session.objects.filter(session_key=previous_session.session_key).delete()
+    except UserSession.DoesNotExist:
+        pass
+
+    # Store the new session key
     UserSession.objects.update_or_create(
         user=user,
         defaults={'session_key': request.session.session_key}
