cfe6e54e46d8870df16b81af05b00b5226cbf700
gujjarrk908 <gujjarrk908@gmail.com>
2026-04-15 11:30:08 +0530

    UI changes for mobile

--- leave_tracker/settings.py
+++ leave_tracker/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME', 'leave_tracker_db'),
         'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'root'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '3306'),
     }
